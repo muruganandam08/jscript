c5f0ef40cbfaae174ae2f8cfa75f16f7c0e5f3cf
muruganandam08 <90926189+muruganandam08@users.noreply.github.com>
2024-08-05 12:08:26 -0300

    Create tracer.js

---
+++ tracer.js
@@ -0,0 +1,27 @@
+// tracer.js
+'use strict';
+
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://<OTEL_COLLECTOR_SERVICE>:4317', // Replace <OTEL_COLLECTOR_SERVICE> with your OpenTelemetry Collector service name
+});
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start()
+  .then(() => console.log('Tracing initialized'))
+  .catch((error) => console.log('Error initializing tracing', error));
+
+// Ensure process can exit
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
